7d334589ec043cbe6f962999b5273995b5d202e8
akshay_manthekar <akky123654@gmail.com>
2023-12-29 23:04:53 +0530

    Sound Service -> Service Locator
    Sound Service class referenced in GameService class

--- Assets/Scripts/Main/GameService.cs
+++ Assets/Scripts/Main/GameService.cs
@@ -1,4 +1,5 @@
 using ServiceLocator.Player;
+using ServiceLocator.Sound;
 using ServiceLocator.Utilities;
 using System.Collections;
 using System.Collections.Generic;
@@ -7,12 +8,18 @@ using UnityEngine;
 public class GameService : GenericMonoSingleton<GameService>
 {
     public PlayerService playerService { get; private set; }
+    public SoundService soundService { get; private set; }
 
     [SerializeField] public PlayerScriptableObject playerScriptableObject;
+    [SerializeField] public SoundScriptableObject soundScriptableObject;
+
+    [SerializeField] private AudioSource audioEffects;
+    [SerializeField] private AudioSource backgroundMusic;
 
     private void Start()
     {
         playerService = new PlayerService(playerScriptableObject);
+        soundService = new SoundService(soundScriptableObject, audioEffects, backgroundMusic);
     }
 
     private void Update()
